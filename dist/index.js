d7b1482612b6e551102392a27f0c0ac5860284aa
0anonymous0guy0@gmail.com <0anonymous0guy0@gmail.com>
2020-06-15 00:08:57 +0530

    Now request to ws are made properly

--- dist/index.js
+++ dist/index.js
@@ -6,10 +6,10 @@ const Player_1 = require("./managers/Player");
 const LavaNode_1 = require("./managers/LavaNode");
 const Queue_1 = require("./managers/Queue");
 module.exports = {
-  LavaClient: LavaClient_1.LavaClient,
-  Player: Player_1.Player,
-  LavaNode: LavaNode_1.LavaNode,
-  Queue: Queue_1.Queue,
-  newTrack: Utils_1.newTrack,
-  newPlaylist: Utils_1.newPlaylist,
+    LavaClient: LavaClient_1.LavaClient,
+    Player: Player_1.Player,
+    LavaNode: LavaNode_1.LavaNode,
+    Queue: Queue_1.Queue,
+    newTrack: Utils_1.newTrack,
+    newPlaylist: Utils_1.newPlaylist,
 };
