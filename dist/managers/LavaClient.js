4da56d66dcead77599d212ef2524633dafd0ba86
0anonymous0guy0@gmail.com <0anonymous0guy0@gmail.com>
2020-06-15 00:09:23 +0530

    Now request to ws are made properly

--- dist/managers/LavaClient.js
+++ dist/managers/LavaClient.js
@@ -6,192 +6,198 @@ const events_1 = require("events");
 const LavaNode_1 = require("./LavaNode");
 const Player_1 = require("./Player");
 class LavaClient extends events_1.EventEmitter {
+  /**
+   * Emitted when a node is connected
+   * @event LavaClient#nodeSuccess
+   * @param {LavaNode} node - The node which connected.
+   */
+  /**
+   * Emitted on a node error
+   * @event LavaClient#nodeError
+   * @param {Node} node - The node which encountered the error.
+   * @param {Error} error - The error message.
+   */
+  /**
+   * Emitted when a node closes
+   * @event LavaClient#nodeClose
+   * @param {Node} node - The node which was closed.
+   * @param {Error} error - The error message.
+   */
+  /**
+   * Emitted when a node reconnects
+   * @event LavaClient#nodeReconnect
+   * @param {Node} node - The node which is reconnecting.
+   */
+  /**
+   * Emitted when a player is created
+   * @event LavaClient#createPlayer
+   * @param {Player} player - The new player.
+   */
+  /**
+   * Emitted when a player is destroyed
+   * @event LavaClient#destroyPlayer
+   * @param {Player} player - The destroyed player.
+   */
+  /**
+   * Emitted when a track ends
+   * @event LavaClient#trackOver
+   * @param {Track} track - The track which ended.
+   * @param {Player} player - Player which was playing the track.
+   */
+  /**
+   * Emitted when a track starts
+   * @event LavaClient#trackPlay
+   * @param {Track} track - The track which started.
+   * @param {Player} player - Player which is playing the track.
+   */
+  /**
+   * Emitted when a track is stuck
+   * @event LavaClient#trackStuck
+   * @param {Track} track - The track which is stuck.
+   * @param {Player} player - Player which was playing the track.
+   * @param {Error} error - The error message.
+   */
+  /**
+   * Emitted when a track encounters an error
+   * @event LavaClient#trackError
+   * @param {Track} track - Track which encountered errored.
+   * @param {Player} player - Player which was playing the track.
+   * @param {Error} error - The error message.
+   */
+  /**
+   * Emitted when a queue ends
+   * @event LavaClient#queueOver
+   * @param {Player} player - Player whose queue ended.
+   */
+  /**
+   * The playlist object
+   * @typedef {Object} Playlist
+   * @property {String} name - Name of playlist.
+   * @property {Number} trackCount - Number of tracks in the playlist.
+   * @property {Number} duration - Total duration of the playlist.
+   * @property {Array<Track>} tracks - The tracks in the playlist.
+   */
+  /**
+   * The track object
+   * @typedef {Object} Track
+   * @property {String} trackString - The 64-bit encoded track.
+   * @property {String} title - Title of the track.
+   * @property {String} identifier - The ID of the track.
+   * @property {String} author - The author of the track.
+   * @property {Number} length - The duration of the track.
+   * @property {Boolean} isStream - Whether to the track is a stream.
+   * @property {String} uri - Track's YouTube url.
+   * @property {*} user - The user who requested the track.
+   * @property {Object} thumbnail - Track's YouTube thumbnails.
+   */
+  /**
+   * The options for the player
+   * @typedef {Object} PlayerOptions
+   * @property {*} guild - The guild where the player is connected to.
+   * @property {*} voiceChannel - The voice channel to connect to.
+   * @property {*} textChannel - The text channel where the player is connected to.
+   * @property {Boolean} [deafen=false] - Whether to deafen the client.
+   * @property {Boolean} [trackRepeat=false] - Whether to repeat the current track.
+   * @property {Boolean} [queueRepeat=false] - Whether to repeat the current queue.
+   * @property {Boolean} [skipOnError=true] - Whether to skip to next song on track stuck/error.
+   */
+  /**
+   * The options for the node
+   * @typedef {Object} NodeOptions
+   * @property {String} host - The host IP or localhost.
+   * @property {Number} port - The port of the node.
+   * @property {String} password - The authorization password of the node.
+   */
+  /**
+   * Creates a new LavaJSClient class instance
+   * @param {*} client - The Discord client.
+   * @param {Array<NodeOptions>} node - The LavaNode to use.
+   */
+  constructor(client, node) {
+    super();
+    this.client = client;
+    this.nodes = node;
+    this.shards = client.ws.shards.size;
+    // Collections
     /**
-     * Emitted when a node is connected
-     * @event LavaClient#nodeSuccess
-     * @param {LavaNode} node - The node which connected.
+     * The node Map collection
+     * @type {Map}
      */
+    this.nodeCollection = new Map();
     /**
-     * Emitted on a node error
-     * @event LavaClient#nodeError
-     * @param {Node} node - The node which encountered the error.
-     * @param {Error} error - The error message.
+     * The player Map collection
+     * @type {Map}
      */
-    /**
-     * Emitted when a node closes
-     * @event LavaClient#nodeClose
-     * @param {Node} node - The node which was closed.
-     * @param {Error} error - The error message.
-     */
-    /**
-     * Emitted when a node reconnects
-     * @event LavaClient#nodeReconnect
-     * @param {Node} node - The node which is reconnecting.
-     */
-    /**
-     * Emitted when a player is created
-     * @event LavaClient#createPlayer
-     * @param {Player} player - The new player.
-     */
-    /**
-     * Emitted when a player is destroyed
-     * @event LavaClient#destroyPlayer
-     * @param {Player} player - The destroyed player.
-     */
-    /**
-     * Emitted when a track ends
-     * @event LavaClient#trackOver
-     * @param {Track} track - The track which ended.
-     * @param {Player} player - Player which was playing the track.
-     */
-    /**
-     * Emitted when a track starts
-     * @event LavaClient#trackPlay
-     * @param {Track} track - The track which started.
-     * @param {Player} player - Player which is playing the track.
-     */
-    /**
-     * Emitted when a track is stuck
-     * @event LavaClient#trackStuck
-     * @param {Track} track - The track which is stuck.
-     * @param {Player} player - Player which was playing the track.
-     * @param {Error} error - The error message.
-     */
-    /**
-     * Emitted when a track encounters an error
-     * @event LavaClient#trackError
-     * @param {Track} track - Track which encountered errored.
-     * @param {Player} player - Player which was playing the track.
-     * @param {Error} error - The error message.
-     */
-    /**
-     * Emitted when a queue ends
-     * @event LavaClient#queueOver
-     * @param {Player} player - Player whose queue ended.
-     */
-    /**
-     * The playlist object
-     * @typedef {Object} Playlist
-     * @property {String} name - Name of playlist.
-     * @property {Number} trackCount - Number of tracks in the playlist.
-     * @property {Number} duration - Total duration of the playlist.
-     * @property {Array<Track>} tracks - The tracks in the playlist.
-     */
-    /**
-     * The track object
-     * @typedef {Object} Track
-     * @property {String} trackString - The 64-bit encoded track.
-     * @property {String} title - Title of the track.
-     * @property {String} identifier - The ID of the track.
-     * @property {String} author - The author of the track.
-     * @property {Number} length - The duration of the track.
-     * @property {Boolean} isStream - Whether to the track is a stream.
-     * @property {String} uri - Track's YouTube url.
-     * @property {*} user - The user who requested the track.
-     * @property {Object} thumbnail - Track's YouTube thumbnails.
-     */
-    /**
-     * The options for the player
-     * @typedef {Object} PlayerOptions
-     * @property {*} guild - The guild where the player is connected to.
-     * @property {*} voiceChannel - The voice channel to connect to.
-     * @property {*} textChannel - The text channel where the player is connected to.
-     * @property {Boolean} [deafen=false] - Whether to deafen the client.
-     * @property {Boolean} [trackRepeat=false] - Whether to repeat the current track.
-     * @property {Boolean} [queueRepeat=false] - Whether to repeat the current queue.
-     * @property {Boolean} [skipOnError=true] - Whether to skip to next song on track stuck/error.
-     */
-    /**
-     * The options for the node
-     * @typedef {Object} NodeOptions
-     * @property {String} host - The host IP or localhost.
-     * @property {Number} port - The port of the node.
-     * @property {String} password - The authorization password of the node.
-     */
-    /**
-     * Creates a new LavaJSClient class instance
-     * @param {*} client - The Discord client.
-     * @param {Array<NodeOptions>} node - The LavaNode to use.
-     */
-    constructor(client, node) {
-        super();
-        this.client = client;
-        this.nodes = node;
-        this.shards = client.ws.shards.size;
-        // Collections
-        /**
-         * The node Map collection
-         * @type {Map}
-         */
-        this.nodeCollection = new Map();
-        /**
-         * The player Map collection
-         * @type {Map}
-         */
-        this.playerCollection = new Map();
-        // Connect the nodes
-        if (!this.nodes || !this.nodes.length)
-            throw new Error("[ClientError] No nodes provided!");
-        for (let x of this.nodes) {
-            if (this.nodeCollection.has(x.host))
-                continue;
-            const newNode = new LavaNode_1.LavaNode(this, x);
-            this.nodeCollection.set(x.host, newNode);
-        }
+    this.playerCollection = new Map();
+    // Connect the nodes
+    if (!this.nodes || !this.nodes.length)
+      throw new Error("[ClientError] No nodes provided!");
+    for (let x of this.nodes) {
+      if (this.nodeCollection.has(x.host)) continue;
+      const newNode = new LavaNode_1.LavaNode(this, x);
+      this.nodeCollection.set(x.host, newNode);
     }
-    /**
-     * Send data to Discord via WebSocket.
-     * @param {Object} data - The data packet to send.
-     */
-    wsSend(data) {
-        if (!this.client)
-            return;
-        const guild = this.client.guilds.cache.get(data.d.guild_id);
-        if (guild && this.shards > 1) {
-            guild.shard.send(data);
-        }
-        else if (guild) {
-            this.client.ws.shards.get(0).send(data);
-        }
+  }
+  /**
+   * Send data to Discord via WebSocket.
+   * @param {Object} data - The data packet to send.
+   */
+  wsSend(data) {
+    if (!this.client) return;
+    const guild = this.client.guilds.cache.get(data.d.guild_id);
+    if (guild && this.shards > 1) {
+      guild.shard.send(data);
+    } else if (guild) {
+      this.client.ws.shards.get(0).send(data);
     }
-    /**
-     * Creates a new LavaJS player or returns old one if player exists
-     * @param {LavaClient} lavaJS - The LavaClient.
-     * @param {PlayerOptions} options - The player options.
-     * @return {Player} player - The new player.
-     */
-    spawnPlayer(lavaJS, options) {
-        if (options.guild && !options.guild.id)
-            options.guild = this.client.guilds.cache.get(options.guild);
-        if (!options.guild)
-            throw new Error(`LavaClient#spawnPlayer() Could not resolve PlayerOptions.guild.`);
-        if (!options.voiceChannel && !options.voiceChannel.id)
-            options.voiceChannel = options.guild.channels.cache.get(options.voiceChannel);
-        if (!options.voiceChannel)
-            throw new Error(`LavaClient#spawnPlayer() Could not resolve PlayerOptions.voiceChannel.`);
-        if (!options.textChannel && !options.textChannel.id)
-            options.textChannel = options.guild.channels.cache.get(options.textChannel);
-        if (!options.textChannel)
-            throw new Error(`LavaClient#spawnPlayer() Could not resolve PlayerOptions.textChannel.`);
-        const oldPlayer = this.playerCollection.get(options.guild.id);
-        if (oldPlayer)
-            return oldPlayer;
-        return new Player_1.Player(this, options, this.optimisedNode);
-    }
-    /**
-     * Returns the node with least resource usage
-     * @return {LavaNode}
-     */
-    get optimisedNode() {
-        const toArray = [...this.nodeCollection.entries()];
-        const sorted = toArray
-            .filter((x) => x[1].online)
-            .sort((a, b) => {
-            const loadA = (a[1].stats.cpu.systemLoad / a[1].stats.cpu.cores) * 100;
-            const loadB = (b[1].stats.cpu.systemLoad / b[1].stats.cpu.cores) * 100;
-            return loadB - loadA;
-        });
-        return sorted[0][1];
-    }
+  }
+  /**
+   * Creates a new LavaJS player or returns old one if player exists
+   * @param {LavaClient} lavaJS - The LavaClient.
+   * @param {PlayerOptions} options - The player options.
+   * @return {Player} player - The new player.
+   */
+  spawnPlayer(lavaJS, options) {
+    if (options.guild && !options.guild.id)
+      options.guild = this.client.guilds.cache.get(options.guild);
+    if (!options.guild)
+      throw new Error(
+        `LavaClient#spawnPlayer() Could not resolve PlayerOptions.guild.`
+      );
+    if (!options.voiceChannel && !options.voiceChannel.id)
+      options.voiceChannel = options.guild.channels.cache.get(
+        options.voiceChannel
+      );
+    if (!options.voiceChannel)
+      throw new Error(
+        `LavaClient#spawnPlayer() Could not resolve PlayerOptions.voiceChannel.`
+      );
+    if (!options.textChannel && !options.textChannel.id)
+      options.textChannel = options.guild.channels.cache.get(
+        options.textChannel
+      );
+    if (!options.textChannel)
+      throw new Error(
+        `LavaClient#spawnPlayer() Could not resolve PlayerOptions.textChannel.`
+      );
+    const oldPlayer = this.playerCollection.get(options.guild.id);
+    if (oldPlayer) return oldPlayer;
+    return new Player_1.Player(this, options, this.optimisedNode);
+  }
+  /**
+   * Returns the node with least resource usage
+   * @return {LavaNode}
+   */
+  get optimisedNode() {
+    const toArray = [...this.nodeCollection.entries()];
+    const sorted = toArray
+      .filter((x) => x[1].online)
+      .sort((a, b) => {
+        const loadA = (a[1].stats.cpu.systemLoad / a[1].stats.cpu.cores) * 100;
+        const loadB = (b[1].stats.cpu.systemLoad / b[1].stats.cpu.cores) * 100;
+        return loadB - loadA;
+      });
+    return sorted[0][1];
+  }
 }
 exports.LavaClient = LavaClient;
